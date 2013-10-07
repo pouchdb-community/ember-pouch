39cd73e40ea68964109da53498669da2085abb28
Ricardo Mendes <ricardo.mendes@ndrive.com>
2013-10-07 01:43:51 +0100

    updates serializer to handle deeply embedded resources

--- hal_serializer.js
+++ hal_serializer.js
@@ -1,49 +1,77 @@
 DS.HALSerializer = DS.RESTSerializer.extend({
   extractSingle: function(store, type, payload, id, requestType) {
     var kind = type.typeKey;
-    var resource = {};
+    var normalizedPayload     = this._normalizeRootResource(kind, payload);
+    var embeddedResources     = this._extractEmbeddedResources(payload);
+    var flatEmbeddedResources = this._flattenRelations(embeddedResources);
+    var merged = _.extend(normalizedPayload, flatEmbeddedResources);
+    var normalizedEmbeddedResources = this._normalizeEmbeddedIds(merged);
 
-    resource = _.extend(
-      this._extractSingleProperties(payload),
-      this._extractEmbeddedIds(payload));
-    resource.id = this._extractResourceId(payload);
-
-    var resource_payload = this._extractEmbeddedResources(payload);
-    resource_payload[kind] = resource;
-
-    return this._super(store, type, resource_payload, resource.id, requestType);
+    return this._super(store, type, normalizedEmbeddedResources, id, requestType);
   },
 
-  _extractSingleProperties: function(payload) {
-    return _.object(
-      _.pairs(payload).reject(function(it) {
-        return it[0] === "_embedded";
-      }).reject(function(it) {
-        return it[0] === "_links";
+  normalizeId: function(hash) {
+    hash.id = hash._links.self.href;
+
+    return hash;
+  },
+
+  _normalizeRootResource: function(type, payload) {
+    var plural =  Ember.String.pluralize(type);
+
+    if (paylay.hasOwnProperty(plural)) return payload;
+
+    var resource = {};
+    resource[Ember.String.pluralize(typeKey)] = [payload];
+
+    return resource;
+  },
+
+  _normalizeEmbeddedIds: function(payload) {
+    var serializerScope = this;
+
+    _.each(payload, function(resources, relation) {
+      var ids = _.map(resources, function(resource) {
+        if (!resource.hasOwnProperty("_embedded")) return resource;
+        serializerScope;
+
+        var ids = _.map(resource._embedded, function(embedded, relation) {
+          if (!_.isArray(embedded)) embedded = [embedded];
+          var mapped = {};
+
+          var mappedIds = _.map(embedded, serializerScope._extractResourceId);
+          mapped[relation] = mappedIds;
+
+          return mapped;
+        });
+        var reduced = _.reduce(ids, function(a,b) { return _.merge(a,b) });
+
+        _.extend(resource, reduced);
+      });
+    });
+
+    return payload;
+  },
+
+  _flattenRelations: function(relations) {
+    var flattened = {};
+
+    _.each(relations, function(embeds) {
+      _.each(embeds, function(relation_embeds, relation) {
+        flattened[relation] = (flattened[relation] || []).concat(relation_embeds);
       })
-    );
+    })
+
+    return flattened;
+  },
+
+  _extractEmbeddedResources: function(payload) {
+    return _.walk.pluckRec(payload, "_embedded");
   },
 
   _extractResourceId: function(payload) {
     var self_link = payload._links.self.href;
 
-    return _.last(self_link.split("/"));
-  },
-
-  _extractEmbeddedIds: function(payload) {
-    var extractId = this._extractResourceId;
-    var embeds    = payload._embedded;
-
-    return _.object(
-      _.pairs(embeds).map(function(it) {
-        return [it[0], it[1].map(extractId)];
-      })
-    );
-  },
-
-  _extractEmbeddedResources: function(payload) {
-    var embeds = payload._embedded;
-
-    return embeds;
+    return self_link;
   }
 });
