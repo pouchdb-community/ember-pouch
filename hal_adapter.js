e5d7ef89cab2de2df59cff8022bfc561c1feb55a
Ricardo Mendes <ricardo.mendes@ndrive.com>
2013-10-07 01:43:21 +0100

    overrides the adapter's find

--- hal_adapter.js
+++ hal_adapter.js
@@ -1,3 +1,7 @@
 DS.HALAdapter = DS.RESTAdapter.extend({
-  defaultSerializer: DS.HALSerializer
+  defaultSerializer: DS.HALSerializer,
+
+  find: function(store, type, id) {
+    return this.ajax(id, 'GET');
+  }
 });
