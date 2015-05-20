c766bb2f8081c5fb71b75c4b3d2bfb45ceca22da
Manuel Wiedenmann <manuel@funkensturm.de>
2015-05-20 12:29:34 +0200

    add embertry scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,7 +2,53 @@ module.exports = {
   scenarios: [
     {
       name: 'default',
-      dependencies: { }
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': '1.0.0-beta.17'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'legacy ember-data 1.0.0.beta.12',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#1.0.0-beta.12'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'legacy ember-data 1.0.0.beta.14.1',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#1.0.0-beta.14.1'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'legacy ember-data 1.0.0.beta.15',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#1.0.0-beta.15'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'legacy ember-data 1.0.0.beta.16.1',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#1.0.0-beta.16.1'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
     },
     {
       name: 'ember-release',
