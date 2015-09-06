70ab4bae2fa1da0087da5938cb05c69792fd8bd4
Matt Marcum <matt@strangelooper.com>
2015-09-05 22:29:52 -0500

    added adapter blueprint

--- blueprints/adapter/files/__root__/__path__/__name__.js
+++ blueprints/adapter/files/__root__/__path__/__name__.js
@@ -1,9 +1,20 @@
 import { Adapter } from 'ember-pouch/index';
 import PouchDB from 'pouchdb';
-import config from 'dummy/config/environment';
+import config from '<%= dasherizedPackageName %>/config/environment';
 
 function createDb() {
   let db = new PouchDB(config.emberpouch.name);
+
+  if (config.emberpouch.remote) {
+      let remote = new PouchDB(config.emberpouch.remote);
+
+      db.sync(remote, {
+        live: true,
+        retry: true
+      });
+  }
+
+  return db;
 }
 
 export default Adapter.extend({
