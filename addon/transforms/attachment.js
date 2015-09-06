8bd0cd9a66cf3e39712995f8f8e0f1e7b6b2daba
Matt Marcum <matt@strangelooper.com>
2015-09-05 21:47:23 -0500

    added attachment transform and unit test

---
+++ addon/transforms/attachment.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Transform.extend({
+  deserialize: function(serialized) {
+    if (Ember.isNone(serialized)) { return null; }
+
+    return Ember.keys(serialized).map(function (attachmentName) {
+      return {
+        name: attachmentName,
+        content_type: serialized[attachmentName]['content_type'],
+        data: serialized[attachmentName]['data']
+      };
+    });
+  },
+
+  serialize: function(deserialized) {
+    if (!Ember.isArray(deserialized)) { return null; }
+
+    return deserialized.reduce(function (acc, attachment) {
+      acc[attachment.name] = {
+        content_type: attachment.content_type,
+        data: attachment.data
+      };
+      return acc;
+    }, {});
+  }
+});
