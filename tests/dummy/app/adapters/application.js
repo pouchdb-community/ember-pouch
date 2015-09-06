a4b642b066d245405f965c24fbbdf51d19185102
Matt Marcum <matt@strangelooper.com>
2015-09-05 22:31:10 -0500

    updated dummy adapter to match blueprint

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -4,6 +4,17 @@ import config from 'dummy/config/environment';
 
 function createDb() {
   let db = new PouchDB(config.emberpouch.name);
+
+  if (config.emberpouch.remote) {
+      let remote = new PouchDB(config.emberpouch.remote);
+
+      db.sync(remote, {
+        live: true,
+        retry: true
+      });
+  }
+
+  return db;
 }
 
 export default Adapter.extend({
