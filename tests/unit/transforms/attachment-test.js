c842ce8eacf9d463584b3875cddc698138a1730c
Stepan Stolyarov <stepan.stolyaro@gmail.com>
2015-12-14 16:54:19 +0600

    `Attachment` transform
    Unit test courtesy of Matt Marcum @mattmarcum

---
+++ tests/unit/transforms/attachment-test.js
@@ -0,0 +1,44 @@
+import { moduleFor, test } from 'ember-qunit';
+
+import Ember from 'ember';
+
+let testSerializedData = {
+  'test.txt': {
+    content_type: 'text/plain',
+    data: 'hello world!'
+  }
+};
+
+let testDeserializedData = [
+  Ember.Object.create({
+    name: 'test.txt',
+    content_type: 'text/plain',
+    data: 'hello world!'
+  })
+];
+
+moduleFor('transform:attachment', 'Unit | Transform | attachment', {});
+
+test('it serializes an attachment', function(assert) {
+  let transform = this.subject();
+  assert.equal(transform.serialize(null), null);
+  assert.equal(transform.serialize(undefined), null);
+
+  let serializedData = transform.serialize(testDeserializedData);
+  let name = testDeserializedData[0].name;
+
+  assert.equal(serializedData[name].content_type, testSerializedData[name].content_type);
+  assert.equal(serializedData[name].data, testSerializedData[name].data);
+});
+
+test('it deserializes an attachment', function(assert) {
+  let transform = this.subject();
+  assert.equal(transform.deserialize(null), null);
+  assert.equal(transform.deserialize(undefined), null);
+
+  let deserializedData = transform.deserialize(testSerializedData);
+
+  assert.equal(deserializedData[0].get('name'), testDeserializedData[0].get('name'));
+  assert.equal(deserializedData[0].get('content_type'), testDeserializedData[0].get('content_type'));
+  assert.equal(deserializedData[0].get('data'), testDeserializedData[0].get('data'));
+});
