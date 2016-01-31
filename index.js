c27b3f0e451b1d6cecf228b8fd87f0a545cc1e66
Stanley Stuart <stanley@stan.li>
2016-01-30 21:09:21 -0600

    add fastboot support for ember-pouch

--- index.js
+++ index.js
@@ -26,15 +26,30 @@ module.exports = {
   },
 
   included: function included(app) {
-    var bowerDir = app.bowerDirectory;
+    if (isFastBoot()) {
+      app.import('vendor/ember-pouch/ember-pouch-fastboot.js');
+    } else {
+      var bowerDir = app.bowerDirectory;
 
-    app.import(bowerDir + '/pouchdb/dist/pouchdb.js');
-    app.import(bowerDir + '/relational-pouch/dist/pouchdb.relational-pouch.js');
-    app.import('vendor/ember-pouch/shim.js', {
-      type: 'vendor',
-      exports: {
-        'pouchdb': [ 'default' ]
-      }
-    });
+      app.import(bowerDir + '/pouchdb/dist/pouchdb.js');
+      app.import(bowerDir + '/relational-pouch/dist/pouchdb.relational-pouch.js');
+      app.import('vendor/ember-pouch/shim.js', {
+        type: 'vendor',
+        exports: {
+          'pouchdb': [ 'default' ]
+        }
+      });
+    }
   }
 };
+
+// Steal *all the things* from github.com/tomdale/ember-network !
+
+var funnel = require('broccoli-funnel');
+
+// Checks to see whether this build is targeting FastBoot. Note that we cannot
+// check this at boot time--the environment variable is only set once the build
+// has started, which happens after this file is evaluated.
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
