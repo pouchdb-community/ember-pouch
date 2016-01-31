3cb223173ca53c3a864ae23db86c79b43c020fa9
Buck Doyle <b@chromatin.ca>
2016-01-31 16:56:32 -0500

    (#16) - Add override so serialiser saves hasMany
    As suggested by @fsmanuel:
    https://github.com/nolanlawson/ember-pouch/issues/16#issuecomment-122651303

--- addon/serializers/pouch.js
+++ addon/serializers/pouch.js
@@ -1,3 +1,19 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({});
+export default DS.RESTSerializer.extend({
+  _shouldSerializeHasMany: function() {
+    return true;
+  },
+
+  // This fixes a failure in Ember Data 1.13 where an empty hasMany
+  // was saving as undefined rather than [].
+  serializeHasMany(snapshot, json, relationship) {
+    this._super.apply(this, arguments);
+
+    const key = relationship.key;
+
+    if (!json[key]) {
+      json[key] = [];
+    }
+  }
+});
