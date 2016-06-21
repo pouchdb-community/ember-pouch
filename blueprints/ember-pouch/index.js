3899980e8abcc22475c4882777e74fcecf7702cc
BT <bernardotolosajr@gmail.com>
2016-06-21 13:25:40 +0800

    implement glue code for query and queryRecord

--- blueprints/ember-pouch/index.js
+++ blueprints/ember-pouch/index.js
@@ -1,4 +1,5 @@
 'use strict';
+    "pouchdb-find": "^0.10.2"
 
 module.exports = {
   normalizeEntityName: function() {},
@@ -6,7 +7,8 @@ module.exports = {
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'pouchdb', target: '^3.5.0' },
-      { name: 'relational-pouch', target: '^1.3.2'}
+      { name: 'relational-pouch', target: '^1.3.2'},
+      { name: 'pouchdb-find', target: '^0.10.2'}
     ]);
   }
 };
