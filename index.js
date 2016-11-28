17ff52e660fb64c02921a7c5a73cf52434c6f71e
Martin Broerse <broerse@martinic.com>
2016-11-28 23:06:28 +0100

    Switched to npm versions

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var stew = require('broccoli-stew');
+
 module.exports = {
   name: 'ember-pouch',
-  included(app) {
-    const bowerDir = app.bowerDirectory;
 
-    app.import(bowerDir + '/pouchdb/dist/pouchdb.js');
-    app.import(bowerDir + '/relational-pouch/dist/pouchdb.relational-pouch.js');
-    app.import(bowerDir + '/pouchdb-find/dist/pouchdb.find.js');
-    app.import('vendor/shims/pouchdb.js', {
+  treeForVendor: function() {
+    var pouchdb = stew.find(path.join(path.dirname(require.resolve('pouchdb')), '..', 'dist'), {
+      destDir: 'pouchdb',
+      files: ['pouchdb.js']
+    });
+
+    var relationalPouch = stew.find(path.join(path.dirname(require.resolve('relational-pouch')), '..', 'dist'), {
+      destDir: 'pouchdb',
+      files: ['pouchdb.relational-pouch.js']
+    });
+
+    var pouchdbFind = stew.find(path.join(path.dirname(require.resolve('pouchdb-find')), '..', 'dist'), {
+      destDir: 'pouchdb',
+      files: ['pouchdb.find.js']
+    });
+
+    var shims = stew.find(__dirname + '/vendor/pouchdb', {
+      destDir: 'pouchdb',
+      files: ['shims.js']
+    });
+
+    return stew.find([
+      pouchdb,
+      relationalPouch,
+      pouchdbFind,
+      shims
+    ]);
+  },
+
+  included(app) {
+    app.import('vendor/pouchdb/pouchdb.js');
+    app.import('vendor/pouchdb/pouchdb.relational-pouch.js');
+    app.import('vendor/pouchdb/pouchdb.find.js');
+    app.import('vendor/pouchdb/shims.js', {
       exports: { 'pouchdb': [ 'default' ]}
     });
   }
