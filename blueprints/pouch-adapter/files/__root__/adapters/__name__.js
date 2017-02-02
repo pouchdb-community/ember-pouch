261c487e77ad4d1d7f744e7a92f7d4abcd5e6893
Ghamdi, Hamzah <ghamha1n@gmail.com>
2017-02-02 22:18:48 +0300

    Update __name__.js

--- blueprints/pouch-adapter/files/__root__/adapters/__name__.js
+++ blueprints/pouch-adapter/files/__root__/adapters/__name__.js
@@ -1,32 +1,9 @@
 import { Adapter } from 'ember-pouch';
-import PouchDB from 'pouchdb';
-import config from '<%= dasherizedPackageName %>/config/environment';
-import Ember from 'ember';
-
-const { assert, isEmpty } = Ember;
-
-function createDb() {
-  let localDb = config.emberPouch.localDb;
-
-  assert('emberPouch.localDb must be set', !isEmpty(localDb));
-
-  let db = new PouchDB(localDb);
-
-  if (config.emberPouch.remoteDb) {
-    let remoteDb = new PouchDB(config.emberPouch.remoteDb);
-
-    db.sync(remoteDb, {
-      live: true,
-      retry: true
-    });
-  }
-
-  return db;
-}
 
 export default Adapter.extend({
+  pouch:Ember.inject.service(),
   init() {
     this._super(...arguments);
-    this.set('db', createDb());
+    this.set('db', this.get('pouch.db'));
   }
 });
