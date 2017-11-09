9288e9c5a78381dd676212a02709391595f26b88
Buck Doyle <b@chromatin.ca>
2017-11-09 09:42:56 -0600

    Fix some jobs on Travis Trusty (#187)
    * Change Travis to use Trusty
    
    * Add output of path to yarn
    
    * Add command to remove yarn
    
    Surely this won’t work. Better to add something to the path?
    
    * Add a fake yarn
    
    Will _this_ work? 🤔
    
    * Move path echoing
    
    * Make yarn executable
    
    * Change to older ember-try
    
    Will this override the Ember CLI one? 🤔
    
    * Remove weird ember-try/yarn workaround
    
    * Remove ember-try version override
    
    * Update Ember CLI and associated projects
    
    * Remove Phantom polyfill
    
    This is unnecessary since Phantom is abandoned for tests.
    
    * Update Babel version
    
    * Return to previously-stated Node version
    
    * Replace ember-try configuration with default

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,53 +2,18 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-2.4-stack',
-      npm: {
-        devDependencies: {
-          'ember-data': '2.4.3',
-          'ember-inflector': '^1.9.4',
-          'ember-source': null,
-          'ember-cli-shims': null
-        }
-      },
-      bower: {
-        dependencies: {
-          'ember': '2.4.6',
-          "ember-cli-shims": "0.1.1"
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.8',
-      npm: {
-        devDependencies: {
-          'ember-data': '2.8.1',
-          'ember-inflector': '^1.9.4',
-          'ember-source': null,
-          'ember-cli-shims': null
-        }
-      },
       bower: {
         dependencies: {
-          'ember': '2.8.3',
-          "ember-cli-shims": "0.1.1"
-        }
-      }
-    },
-    {
-      name: 'ember-2.10-stack',
-      npm: {
-        devDependencies: {
-          'ember-data': '2.10.0',
-          'ember-inflector': '^1.9.4',
-          'ember-source': null,
-          'ember-cli-shims': null
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
         }
       },
-      bower: {
-        dependencies: {
-          'ember': '2.10.2',
-          "ember-cli-shims": "0.1.1"
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     },
@@ -56,48 +21,63 @@ module.exports = {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-data': '2.12.2',
-          'ember-inflector': '^1.9.4',
-          'ember-source': '2.12.2',
-          'ember-cli-shims': "^1.1.0"
+          'ember-source': '~2.12.0'
         }
-      },
-    },
-    {
-      name: 'ember-2.14-stack',
-      npm: {
-        devDependencies: {
-          'ember-data': '2.14.10',
-          'ember-source': '2.14.1',
-        }
-      },
+      }
     },
     {
       name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-data': 'latest',
-          'ember-source': 'latest',
-        },
+          'ember-source': null
+        }
       }
     },
     {
       name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-data': 'beta',
-          'ember-source': 'beta',
-        },
+          'ember-source': null
+        }
       }
     },
-//    {
-//      name: 'ember-canary',
-//      npm: {
-//        devDependencies: {
-//          'ember-data': 'components/ember-data#canary',
-//          'ember-source': 'components/ember#canary',
-//        },
-//      },
-//    }
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
+      }
+    }
   ]
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,9 +14,6 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import('bower_components/phantomjs-polyfill-object-assign/object-assign-polyfill.js', {
-    type: 'test'
-  });
 
   return app.toTree();
 };
