97cc46dcedf7cd204010a7dcc75535ff22a988d1
Buck Doyle <b@chromatin.ca>
2017-11-10 12:11:29 -0600

    Restore ember-try configuration for Ember Data (#201)

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,10 +2,28 @@
 module.exports = {
   scenarios: [
     {
+      name: 'ember-2.4-stack',
+      npm: {
+        devDependencies: {
+          'ember-data': '2.4.3',
+          'ember-inflector': '^1.9.4',
+          'ember-source': null,
+          'ember-cli-shims': null
+        }
+      },
+      bower: {
+        dependencies: {
+          'ember': '2.4.6',
+          'ember-cli-shims': '0.1.1'
+        }
+      }
+    },
+    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-8'
+          'ember': 'components/ember#lts-2-8',
+          'ember-cli-shims': '0.1.1'
         },
         resolutions: {
           'ember': 'lts-2-8'
@@ -13,7 +31,10 @@ module.exports = {
       },
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-data': '2.8.1',
+          'ember-inflector': '^1.9.4',
+          'ember-source': null,
+          'ember-cli-shims': null
         }
       }
     },
@@ -21,9 +42,21 @@ module.exports = {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0'
+          'ember-data': '2.12.2',
+          'ember-inflector': '^1.9.4',
+          'ember-source': '2.12.2',
+          'ember-cli-shims': '^1.1.0'
         }
       }
+    },
+    {
+      name: 'ember-2.14-stack',
+      npm: {
+        devDependencies: {
+          'ember-data': '2.14.10',
+          'ember-source': '2.14.1',
+        }
+      },
     },
     {
       name: 'ember-release',
@@ -37,6 +70,7 @@ module.exports = {
       },
       npm: {
         devDependencies: {
+          'ember-data': 'latest',
           'ember-source': null
         }
       }
@@ -53,6 +87,7 @@ module.exports = {
       },
       npm: {
         devDependencies: {
+          'ember-data': 'beta',
           'ember-source': null
         }
       }
@@ -69,6 +104,7 @@ module.exports = {
       },
       npm: {
         devDependencies: {
+          'ember-data': 'canary',
           'ember-source': null
         }
       }
