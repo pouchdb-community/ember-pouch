7ff39c4bf866e324f979fc198ae311278d8c4ebd
Buck Doyle <b@chromatin.ca>
2017-11-11 13:27:42 -0600

    Change ember-try for Ember Data canary

--- config/ember-try.js
+++ config/ember-try.js
@@ -96,15 +96,17 @@ module.exports = {
       name: 'ember-canary',
       bower: {
         dependencies: {
-          'ember': 'components/ember#canary'
+          'ember': 'components/ember#canary',
+          'ember-data': 'components/ember-data#canary'
         },
         resolutions: {
-          'ember': 'canary'
+          'ember': 'canary',
+          'ember-data': 'canary'
         }
       },
       npm: {
         devDependencies: {
-          'ember-data': 'canary',
+          'ember-data': null,
           'ember-source': null
         }
       }
