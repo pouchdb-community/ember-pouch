5f68d9e2fa84422dda4e9f84823bf759bf2239d9
segoja <segoja@gmail.com>
2022-05-20 09:26:25 +0200

    one more

--- addon/pouchdb.js
+++ addon/pouchdb.js
@@ -1,6 +1,6 @@
-import PouchDB from 'pouchdb-browser';
-import PouchDBFind from 'pouchdb-find';
-import PouchDBRelational from 'relational-pouch';
+import { PouchDB } from 'pouchdb-browser';
+import { PouchDBFind } from 'pouchdb-find';
+import { PouchDBRelational } from 'relational-pouch';
 
 PouchDB.plugin(PouchDBFind).plugin(PouchDBRelational);
 
