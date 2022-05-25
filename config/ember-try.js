dff9bfd5eaea552eb47f504c7a3a9b293fd8f912
segoja <segoja@gmail.com>
2022-05-25 17:51:46 +0200

    updated ember try and packages

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,48 +7,9 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-2.4-stack',
-        npm: {
-          devDependencies: {
-            'ember-data': '2.4.3',
-            'ember-inflector': '^1.9.4',
-            'ember-source': null,
-            'ember-cli-shims': null,
-          },
-        },
-        bower: {
-          dependencies: {
-            ember: '2.4.6',
-            'ember-cli-shims': '0.1.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-2.8',
-        bower: {
-          dependencies: {
-            ember: 'components/ember#lts-2-8',
-            'ember-cli-shims': '0.1.1',
-          },
-          resolutions: {
-            ember: 'lts-2-8',
-          },
-        },
-        npm: {
-          devDependencies: {
-            'ember-data': '2.8.1',
-            'ember-inflector': '^1.9.4',
-            'ember-source': null,
-            'ember-cli-shims': null,
-          },
-        },
-      },
-      {
         name: 'ember-lts-2.12',
         env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
         },
         npm: {
           devDependencies: {
@@ -56,54 +17,48 @@ module.exports = async function () {
             'ember-data': '2.12.2',
             'ember-inflector': '^1.9.4',
             'ember-source': '2.12.2',
-            'ember-cli-shims': '^1.1.0',
-          },
-        },
+            'ember-cli-shims': '^1.1.0'
+          }
+        }
       },
       {
         name: 'ember-2.14-stack',
         env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
         },
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1',
             'ember-data': '~2.14.0',
             'ember-source': '~2.14.0',
-          },
+          }
         },
       },
       {
         name: 'ember-lts-2.16',
         env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
         },
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1',
             'ember-data': '~2.16.0',
             'ember-source': '~2.16.0',
-          },
-        },
+          }
+        }
       },
       {
         name: 'ember-lts-2.18',
         env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
         },
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1',
             'ember-data': '~2.18.0',
             'ember-source': '~2.18.0',
-          },
-        },
+          }
+        }
       },
       {
         name: 'ember-lts-3.4',
@@ -111,8 +66,8 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': '~3.4.0',
             'ember-data': '~3.4.0',
-          },
-        },
+          }
+        }
       },
       {
         name: 'ember-lts-3.8',
@@ -120,8 +75,8 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': '~3.8.0',
             'ember-data': '~3.8.0',
-          },
-        },
+          }
+        }
       },
       {
         name: 'ember-lts-3.12',
@@ -129,22 +84,24 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': '~3.12.0',
             'ember-data': '~3.12.0',
-          },
-        },
+          }
+        }
       },
       {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
             'ember-source': '~3.16.0',
-          },
-        },
+            'ember-data': '~3.16.0',
+          }
+        }
       },
       {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
             'ember-source': '~3.24.3',
+            'ember-data': '~3.24.0',
           },
         },
       },
@@ -153,6 +110,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-data': '~3.27.1',
           },
         },
       },
