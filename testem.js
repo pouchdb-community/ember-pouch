a0df870567a0bde219bf6d1d1a3dab51fa117f79
segoja <segoja@gmail.com>
2022-05-25 21:34:39 +0200

    testem set for Travis

--- testem.js
+++ testem.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 'use strict';
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
@@ -8,13 +9,13 @@ module.exports = {
   browser_start_timeout: 120,
   browser_args: {
     Chrome: {
-      ci: [
+      mode: 'ci',
+      args: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
+        '--disable-gpu',
         '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
       ].filter(Boolean),
