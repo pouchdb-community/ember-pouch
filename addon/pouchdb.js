563b94d98dd82a5e9b7d3625cb6d507939aa8cce
Joren l'Ami <joren@distinct.nl>
2022-06-20 10:03:36 +0200

    added indexeddb adapter instead of idb

--- addon/pouchdb.js
+++ addon/pouchdb.js
@@ -1,7 +1,18 @@
-import PouchDB from 'pouchdb-browser';
+import PouchDB from 'pouchdb-core';
 import PouchDBFind from 'pouchdb-find';
 import PouchDBRelational from 'relational-pouch';
+import indexeddb from 'pouchdb-adapter-indexeddb';
+import HttpPouch from 'pouchdb-adapter-http';
+import mapreduce from 'pouchdb-mapreduce';
+import replication from 'pouchdb-replication';
 
-PouchDB.plugin(PouchDBFind).plugin(PouchDBRelational);
+PouchDB
+  .plugin(PouchDBFind)
+  .plugin(PouchDBRelational)
+  .plugin(indexeddb)
+  .plugin(HttpPouch)
+  .plugin(mapreduce)
+  .plugin(replication)
+  ;
 
 export default PouchDB;
